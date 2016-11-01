f26a33f6c09dabb8ca32835d38ab4202e87ac751
Cédric Belin <cedric@belin.io>
2016-11-01 01:35:40 +0100

    The plug-in options are optional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import {Minifier} from './minifier';
+export * from './minifier';
 
 /**
  * Create a new instance of the plug-in.
- * @param {object} options The plug-in options.
+ * @param {object} [options] The plug-in options.
  * @return {Minifier} Tthe newly created instance.
  */
-export function phpMinify(options) {
+export function phpMinify(options = {}) {
   return new Minifier(options);
 }
-
