3cc544378c766691152f24435aaf461dcae8f664
Cédric Belin <cedric@belin.io>
2016-11-01 13:26:55 +0100

    Ported the Promise-based APIs to Observables

--- src/minifier.js
+++ src/minifier.js
@@ -1,15 +1,15 @@
-const childProcess = require('child_process');
-const path = require('path');
-const pkg = require('../package.json');
-const portFinder = require('portfinder');
-const request = require('superagent');
-const {Transform} = require('stream');
+import child from 'child_process';
+import path from 'path';
+import {Observable} from 'rxjs';
+import * as pkg from '../package.json';
+import portFinder from 'portfinder';
+import superagent from 'superagent';
+import {Transform} from 'stream';
 
 /**
- * Removes PHP comments and whitespace by applying the [`php_strip_whitespace()`](http://php.net/manual/en/function.php-strip-whitespace.php) function.
- * @augments stream.Transform
+ * Removes PHP comments and whitespace by applying the [`php_strip_whitespace()`](https://secure.php.net/manual/en/function.php-strip-whitespace.php) function.
  */
-module.exports = class Minifier extends Transform {
+export class Minifier extends Transform {
 
   /**
    * Initializes a new instance of the class.
@@ -19,83 +19,76 @@ module.exports = class Minifier extends Transform {
     super({objectMode: true});
 
     /**
-     * The minifier settings.
-     * @type {object}
+     * The path to the PHP executable.
+     * @type {string}
      */
-    this._options = {
-      binary: typeof options.binary == 'string' ? options.binary : 'php',
-      silent: typeof options.silent == 'boolean' ? options.silent : false
-    };
+    this.binary = typeof options.binary == 'string' ? path.normalize(options.binary) : 'php';
+
+    /**
+     * Value indicating whether to silent the plug-in output.
+     * @type {boolean}
+     */
+    this.silent = typeof options.silent == 'boolean' ? options.silent : false;
 
     /**
      * The underlying PHP process.
      * @type {object}
      */
     this._phpServer = null;
-
-    // Initialize the instance.
-    Object.assign(this._options, options);
-    this._options.binary = path.normalize(this._options.binary);
   }
 
   /**
    * Terminates the underlying PHP process: stops the server from accepting new connections.
-   * @returns {Promise} Completes when the PHP process is finally terminated.
+   * @return {Observable} Completes when the PHP process is finally terminated.
    */
   close() {
-    if(!this._phpServer) return Promise.reject(new Error('The PHP process is not started.'));
+    if (!this._phpServer) return Observable.throw(new Error('The PHP process is not started.'));
 
-    return new Promise(resolve => {
+    return new Observable(observer => {
       this._phpServer.process.kill();
       this._phpServer = null;
-      resolve();
+      observer.complete();
     });
   }
 
   /**
    * Starts the underlying PHP process: begins accepting connections.
-   * @returns {Promise} Completes when the PHP process has been started.
+   * @return {Observable} Completes when the PHP process has been started.
    */
   listen() {
-    if(this._phpServer) return Promise.reject(new Error('The PHP process is already started.'));
+    if (this._phpServer) return Observable.throw(new Error('The PHP process is already started.'));
 
-    return new Promise((resolve, reject) => portFinder.getPort((err, port) => {
-      if(err) reject(err);
-      else {
-        let host = `127.0.0.1:${port}`;
-        let args = ['-S', host, '-t', path.join(__dirname, '../web')];
+    return Observable.bindNodeCallback(portFinder.getPort).map(port => {
+      let host = `127.0.0.1:${port}`;
+      let args = ['-S', host, '-t', path.join(__dirname, '../web')];
 
-        this._phpServer = {
-          host,
-          process: childProcess.spawn(this._options.binary, args)
-        };
-
-        this.once('end', () => this.close());
-        resolve();
-      }
-    }));
+      this._phpServer = {host, process: child.spawn(this.binary, args)};
+      this.once('end', () => this.close().subscribe());
+    });
   }
 
   /**
    * Transforms input and produces output.
-   * @param {vinyl.File} file The chunk to be transformed.
+   * @param {File} file The chunk to be transformed.
    * @param {string} encoding The encoding type if the chunk is a string.
    * @param {function} callback The function to invoke when the supplied chunk has been processed.
    */
   _transform(file, encoding, callback) {
-    let promise = this._phpServer ? Promise.resolve() : this.listen();
-    promise
-      .then(() => {
-        if(!this._options.silent) console.log(`Minifying: ${file.path}`);
-        return request(`http://${this._phpServer.host}/index.php`).query({file: file.path});
-      })
-      .then(res => {
-        if(res.error) throw new Error(`HTTP error ${res.status}`);
+    if (!this.silent) console.log(`Minifying: ${file.path}`);
+
+    let request = superagent
+      .get(`http://${this._phpServer.host}/index.php`)
+      .query({file: file.path})
+      .bind(superagent);
+
+    Observable.bindNodeCallback(request)
+      .map(res => {
         file.contents = Buffer.from(res.text, encoding);
-        callback(null, file);
+        return file;
       })
-      .catch(err =>
-        callback(new Error(`[${pkg.name}] ${err}`))
+      .subscribe(
+        file => callback(null, file),
+        err => callback(new Error(`[${pkg.name}] ${err}`))
       );
   }
-};
+}
