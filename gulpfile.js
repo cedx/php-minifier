89689555419d339b2cb23b8dc65d0c7d2cee24ba
Cédric Belin <cedric@belin.io>
2017-02-17 13:47:01 +0100

    Updated the Coveralls dependency

--- gulpfile.js
+++ gulpfile.js
@@ -48,9 +48,8 @@ gulp.task('clean', () => del('var/**/*'));
  * Sends the results of the code coverage.
  */
 gulp.task('coverage', ['test'], () => {
-  let command = process.platform == 'win32' ? 'type' : 'cat';
   let executable = path.join('node_modules/.bin', process.platform == 'win32' ? 'coveralls.cmd' : 'coveralls');
-  return _exec(`${command} var/lcov.info | ${executable}`);
+  return _exec(`${executable} --file=var/lcov.info`);
 });
 
 /**
