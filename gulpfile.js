0f3b935a2a4b9715482ac3ab5b91db736cd185ee
Cédric Belin <cedric@belin.io>
2017-04-30 20:14:11 +0200

    Added support for NSP security report

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,13 @@ gulp.task('clean', () => del('var/**/*'));
 gulp.task('coverage', ['test'], () => _exec('node_modules/.bin/coveralls', ['--file=var/lcov.info']));
 
 /**
+ * Checks the package dependencies.
+ */
+gulp.task('deps', ['deps:outdated', 'deps:security']);
+gulp.task('deps:outdated', () => gulp.src('package.json').pipe(david()));
+gulp.task('deps:security', () => _exec('node_modules/.bin/nsp', ['check']));
+
+/**
  * Builds the documentation.
  */
 gulp.task('doc', async () => {
@@ -54,11 +61,6 @@ gulp.task('lint', () => gulp.src(['*.js', 'src/**/*.js', 'test/**/*.js'])
   .pipe(eslint())
   .pipe(eslint.format())
 );
-
-/**
- * Checks the package dependencies.
- */
-gulp.task('outdated', () => gulp.src('package.json').pipe(david()));
 
 /**
  * Runs the unit tests.
