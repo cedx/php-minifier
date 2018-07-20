404cca216f29f7ddd117b9733012ac9823f448c0
Cédric Belin <cedric@belin.io>
2018-07-20 22:17:48 +0200

    Removed the dependency on Node Security Platform

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('coverage', () => _exec('node_modules/.bin/coveralls', ['var/lcov.info
  * Checks the package dependencies.
  */
 gulp.task('deps:outdated', () => gulp.src('package.json').pipe(david()));
-gulp.task('deps:security', () => _exec('node_modules/.bin/nsp', ['check']));
+gulp.task('deps:security', () => _exec('npm', ['audit']));
 gulp.task('deps', gulp.series('deps:outdated', 'deps:security'));
 
 /**
@@ -34,10 +34,13 @@ gulp.task('doc', gulp.series('doc:api', 'doc:web'));
 /**
  * Fixes the coding standards issues.
  */
-gulp.task('fix', () => gulp.src(['*.js', 'example/*.js', 'lib/**/*.js', 'test/**/*.js'], {base: '.'})
+gulp.task('fix:js', () => gulp.src(['*.js', 'example/*.js', 'lib/**/*.js', 'test/**/*.js'], {base: '.'})
   .pipe(eslint({fix: true}))
   .pipe(gulp.dest('.'))
 );
+
+gulp.task('fix:security', () => _exec('npm', ['audit', 'fix']));
+gulp.task('fix', gulp.series('fix:js', 'fix:security'));
 
 /**
  * Performs static analysis of source code.
