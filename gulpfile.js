fdd82ed968d4e85a0e586e78de99e2deee29a105
Cédric Belin <cedric@belin.io>
2018-08-12 22:53:06 +0200

    Updated the dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('lint', () => _exec('node_modules/.bin/tslint', sources));
 /**
  * Runs the unit tests.
  */
-gulp.task('test', () => _exec('node_modules/.bin/nyc', [normalize('node_modules/.bin/mocha'), 'test/**/*.ts']));
+gulp.task('test', () => _exec('node_modules/.bin/nyc', [normalize('node_modules/.bin/mocha')]));
 
 /**
  * Upgrades the project to the latest revision.
@@ -55,7 +55,7 @@ gulp.task('upgrade', async () => {
   await _exec('git', ['reset', '--hard']);
   await _exec('git', ['fetch', '--all', '--prune']);
   await _exec('git', ['pull', '--rebase']);
-  await _exec('npm', ['install']);
+  await _exec('npm', ['install', '--ignore-scripts']);
   return _exec('npm', ['update']);
 });
 
