602ec26243b0652c9331125b51bc9e0b693d47ce
Cédric Belin <cedric@belin.io>
2018-08-15 02:25:46 +0200

    Updated the `upgrade` build task

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,7 @@ gulp.task('upgrade', async () => {
   await _exec('git', ['reset', '--hard']);
   await _exec('git', ['fetch', '--all', '--prune']);
   await _exec('git', ['pull', '--rebase']);
+  await _exec('npm', ['install', '--ignore-scripts']);
   return _exec('npm', ['update', '--dev']);
 });
 
