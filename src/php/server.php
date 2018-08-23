86bdb95746e6a7d51af6feba659675aa54a4a581
Cédric Belin <cedric@belin.io>
2018-08-23 21:57:27 +0200

    Do not expose PHP internals

--- src/php/server.php
+++ src/php/server.php
@@ -8,20 +8,6 @@ namespace Gulp\PhpMinify;
 class Server {
 
   /**
-   * Handles the PHP execution errors such as warnings and notices.
-   * @param int $severity The level of the error raised.
-   * @param string $message The error message.
-   * @param string $file The filename that the error was raised in.
-   * @param int $line The line number the error was raised at.
-   * @return bool Whether the error was handled.
-   * @throws \ErrorException When an error occurred.
-   */
-  function handleError(int $severity, string $message, string $file = __FILE__, int $line = __LINE__): bool {
-    if (error_reporting() & $severity) throw new \ErrorException($message, 0, $severity, $file, $line);
-    return false;
-  }
-
-  /**
    * Runs the application.
    * @param array $args The request parameters.
    */
@@ -32,12 +18,26 @@ class Server {
   }
 
   /**
+   * Handles the PHP execution errors such as warnings and notices.
+   * @param int $severity The level of the error raised.
+   * @param string $message The error message.
+   * @param string $file The filename that the error was raised in.
+   * @param int $line The line number the error was raised at.
+   * @return bool Whether the error was handled.
+   * @throws \ErrorException When an error occurred.
+   */
+  private function handleError(int $severity, string $message, string $file = __FILE__, int $line = __LINE__): bool {
+    if (error_reporting() & $severity) throw new \ErrorException($message, 0, $severity, $file, $line);
+    return false;
+  }
+
+  /**
    * Processes the specified request body.
    * @param array $args The request sent by a client.
    * @return string The stripped source code corresponding to the provided file.
    * @throws \Exception The requirements are not met, or an error occurred.
    */
-  function processRequest(array $args): string {
+  private function processRequest(array $args): string {
     if (!isset($args['file']) || !mb_strlen($args['file'])) throw new \LogicException('Bad Request', 400);
     if (!is_file($args['file'])) throw new \RuntimeException('Not Found', 404);
 
@@ -51,7 +51,7 @@ class Server {
    * @param string $body The response body to send to the client.
    * @param int $status The status code of the response.
    */
-  function sendResponse(string $body, int $status = 200): void {
+  private function sendResponse(string $body, int $status = 200): void {
     http_response_code($status);
     header('Content-Length: '.strlen($body));
     header('Content-Type: text/plain; charset='.mb_internal_encoding());
