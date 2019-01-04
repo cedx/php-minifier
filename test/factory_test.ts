d07cc5716551732e6979c07386bbd745ba990e74
Cédric Belin <cedric@belin.io>
2019-01-04 11:57:55 +0100

    Fixed compilation errors

--- test/factory_test.ts
+++ test/factory_test.ts
@@ -22,10 +22,8 @@ import {Minifier, phpMinify, TransformMode} from '../src';
       silent: true
     });
 
-    const executable = process.platform == 'win32' ? '\\usr\\local\\bin\\php' : '/usr/local/bin/php';
-    expect(minifier.silent).to.be.true;
-
     // @ts-ignore: `_transformer` is a private property.
-    expect(minifier._transformer).to.equal(`fast:${executable}`);
+    expect(minifier._transformer).to.equal('fast:/usr/local/bin/php');
+    expect(minifier.silent).to.be.true;
   }
 }
