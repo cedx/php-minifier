de6ff3504d4e70fc8f2eaef960713d31f8d94262
Cédric Belin <cedric@belin.io>
2019-04-30 01:16:58 +0200

    Updated the linter settings

--- etc/typedoc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  excludePrivate: true,
-  gaID: process.env.GOOGLE_ANALYTICS_ID,
-  gitRevision: 'master',
-  mode: 'modules',
-  name: 'Gulp-PHP-Minify',
-  out: require('path').join(__dirname, '../doc/api')
-};
