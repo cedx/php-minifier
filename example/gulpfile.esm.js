848c94f7788cc7171543c4c37bb4c5f37a49ccae
Cédric Belin <cedric@belin.io>
2019-04-30 01:17:14 +0200

    Updated the example code

--- example/gulpfile.esm.js
+++ example/gulpfile.esm.js
@@ -1,13 +1,12 @@
-'use strict';
-const {phpMinify, TransformMode} = require('@cedx/gulp-php-minify');
-const {dest, src, task} = require('gulp');
+import {phpMinify, TransformMode} from '@cedx/gulp-php-minify';
+import gulp from 'gulp';
 
 /** Compresses a given set of PHP scripts. */
-task('compress:php', () => src('path/to/**/*.php', {read: false})
+gulp.task('compress:php', () => gulp.src('path/to/**/*.php', {read: false})
   .pipe(phpMinify({
     binary: process.platform == 'win32' ? 'C:\\Program Files\\PHP\\php.exe' : '/usr/bin/php',
     mode: process.platform == 'win32' ? TransformMode.safe : TransformMode.fast,
     silent: process.stdout.isTTY
   }))
-  .pipe(dest('path/to/out'))
+  .pipe(gulp.dest('path/to/out'))
 );
