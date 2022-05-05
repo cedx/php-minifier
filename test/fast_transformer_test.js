b226331052bc0fdcfdabe9a72d3a8411b243baf5
Cédric Belin <cedric@belin.io>
2022-05-06 00:54:50 +0200

    Updated the dependencies

--- test/fast_transformer_test.js
+++ test/fast_transformer_test.js
@@ -1,72 +1,60 @@
-import {strict as assert} from 'assert';
-import {FastTransformer} from '../lib/index.js';
+import assert from "node:assert/strict";
+import {FastTransformer} from "../lib/index.js";
 
-/** Tests the features of the `FastTransformer` class. */
-describe('FastTransformer', function() {
-  this.retries(2);
-  this.timeout(30000);
+/**
+ * Tests the features of the {@link FastTransformer} class.
+ */
+describe("FastTransformer", /** @this {Mocha.Suite} */ function() {
+	this.retries(2);
+	this.timeout(30_000);
 
-  describe('.listening', () => {
-    const transformer = new FastTransformer;
+	describe(".close()", () => {
+		const transformer = new FastTransformer;
 
-    it('should return whether the server is listening', async () => {
-      assert.equal(transformer.listening, false);
+		it("should complete without any error", () => {
+			assert.doesNotReject(transformer.listen());
+			assert.doesNotReject(transformer.close());
+		});
 
-      await transformer.listen();
-      assert(transformer.listening);
+		it("should be callable multiple times", () => {
+			assert.doesNotReject(transformer.close());
+			assert.doesNotReject(transformer.close());
+		});
+	});
 
-      await transformer.close();
-      assert.equal(transformer.listening, false);
-    });
-  });
+	describe(".listen()", () => {
+		const transformer = new FastTransformer;
+		after(() => transformer.close());
 
-  describe('.close()', () => {
-    const transformer = new FastTransformer;
+		it("should complete without any error", () => {
+			assert.doesNotReject(transformer.listen());
+		});
 
-    it('should complete without any error', () => {
-      assert.doesNotReject(transformer.listen());
-      assert.doesNotReject(transformer.close());
-    });
+		it("should be callable multiple times", () => {
+			assert.doesNotReject(transformer.listen());
+			assert.doesNotReject(transformer.listen());
+		});
+	});
 
-    it('should be callable multiple times', () => {
-      assert.doesNotReject(transformer.close());
-      assert.doesNotReject(transformer.close());
-    });
-  });
+	describe(".transform()", () => {
+		const script = "test/fixtures/sample.php";
+		const transformer = new FastTransformer;
+		after(() => transformer.close());
 
-  describe('.listen()', () => {
-    const transformer = new FastTransformer;
-    after(() => transformer.close());
+		it("should remove the inline comments", async () => {
+			assert((await transformer.transform(script)).includes("<?= 'Hello World!' ?>"));
+		});
 
-    it('should complete without any error', () => {
-      assert.doesNotReject(transformer.listen());
-    });
+		it("should remove the multi-line comments", async () => {
+			assert((await transformer.transform(script)).includes("namespace dummy; class Dummy"));
+		});
 
-    it('should be callable multiple times', () => {
-      assert.doesNotReject(transformer.listen());
-      assert.doesNotReject(transformer.listen());
-    });
-  });
+		it("should remove the single-line comments", async () => {
+			assert((await transformer.transform(script)).includes("$className = get_class($this); return $className;"));
+		});
 
-  describe('.transform()', () => {
-    const script = 'test/fixtures/sample.php';
-    const transformer = new FastTransformer;
-    after(() => transformer.close());
-
-    it('should remove the inline comments', async () => {
-      assert((await transformer.transform(script)).includes("<?= 'Hello World!' ?>"));
-    });
-
-    it('should remove the multi-line comments', async () => {
-      assert((await transformer.transform(script)).includes('namespace dummy; class Dummy'));
-    });
-
-    it('should remove the single-line comments', async () => {
-      assert((await transformer.transform(script)).includes('$className = get_class($this); return $className;'));
-    });
-
-    it('should remove the whitespace', async () => {
-      assert((await transformer.transform(script)).includes('__construct() { }'));
-    });
-  });
+		it("should remove the whitespace", async () => {
+			assert((await transformer.transform(script)).includes("__construct() { }"));
+		});
+	});
 });
--- test/safe_transformer_test.js
+++ test/safe_transformer_test.js
@@ -1,43 +1,45 @@
-import {strict as assert} from 'assert';
-import {SafeTransformer} from '../lib/index.js';
+import assert from "node:assert/strict";
+import {SafeTransformer} from "../lib/index.js";
 
-/** Tests the features of the `SafeTransformer` class. */
-describe('SafeTransformer', function() {
-  this.retries(2);
-  this.timeout(30000);
+/**
+ * Tests the features of the {@link SafeTransformer} class.
+ */
+describe("SafeTransformer", /** @this {Mocha.Suite} */ function() {
+	this.retries(2);
+	this.timeout(30_000);
 
-  describe('.close()', () => {
-    const transformer = new SafeTransformer;
+	describe(".close()", () => {
+		const transformer = new SafeTransformer;
 
-    it('should complete without any error', () => {
-      assert.doesNotReject(transformer.close());
-    });
+		it("should complete without any error", () => {
+			assert.doesNotReject(transformer.close());
+		});
 
-    it('should be callable multiple times', () => {
-      assert.doesNotReject(transformer.close());
-      assert.doesNotReject(transformer.close());
-    });
-  });
+		it("should be callable multiple times", () => {
+			assert.doesNotReject(transformer.close());
+			assert.doesNotReject(transformer.close());
+		});
+	});
 
-  describe('.transform()', () => {
-    const script = 'test/fixtures/sample.php';
-    const transformer = new SafeTransformer;
-    after(() => transformer.close());
+	describe(".transform()", () => {
+		const script = "test/fixtures/sample.php";
+		const transformer = new SafeTransformer;
+		after(() => transformer.close());
 
-    it('should remove the inline comments', async () => {
-      assert((await transformer.transform(script)).includes("<?= 'Hello World!' ?>"));
-    });
+		it("should remove the inline comments", async () => {
+			assert((await transformer.transform(script)).includes("<?= 'Hello World!' ?>"));
+		});
 
-    it('should remove the multi-line comments', async () => {
-      assert((await transformer.transform(script)).includes('namespace dummy; class Dummy'));
-    });
+		it("should remove the multi-line comments", async () => {
+			assert((await transformer.transform(script)).includes("namespace dummy; class Dummy"));
+		});
 
-    it('should remove the single-line comments', async () => {
-      assert((await transformer.transform(script)).includes('$className = get_class($this); return $className;'));
-    });
+		it("should remove the single-line comments", async () => {
+			assert((await transformer.transform(script)).includes("$className = get_class($this); return $className;"));
+		});
 
-    it('should remove the whitespace', async () => {
-      assert((await transformer.transform(script)).includes('__construct() { }'));
-    });
-  });
+		it("should remove the whitespace", async () => {
+			assert((await transformer.transform(script)).includes("__construct() { }"));
+		});
+	});
 });
