93e166d1c4a62c52d2d14a08320902a524a1e7e4
Cédric Belin <cedric@belin.io>
2022-05-06 00:54:37 +0200

    Ported the `Minifier` class to JS

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,12 +1,17 @@
-import {phpMinify, TransformMode} from '@cedx/gulp-php-minify';
-import gulp from 'gulp';
+import process from "node:process";
+import phpMinify from "@cedx/gulp-php-minify";
+import gulp from "gulp";
 
 /** Compresses a given set of PHP scripts. */
-gulp.task('compress:php', () => gulp.src('path/to/**/*.php', {read: false})
-  .pipe(phpMinify({
-    binary: process.platform == 'win32' ? String.raw`C:\Program Files\PHP\php.exe` : '/usr/bin/php',
-    mode: process.platform == 'win32' ? TransformMode.safe : TransformMode.fast,
-    silent: process.stdout.isTTY
-  }))
-  .pipe(gulp.dest('path/to/out'))
-);
+export function compressPhp() {
+	const isWindows = process.platform == "win32";
+	const options = {
+		binary: isWindows ? "C:\\Program Files\\PHP\\php.exe" : "/usr/bin/php",
+		mode: isWindows ? "safe" : "fast",
+		silent: process.stdout.isTTY
+	};
+
+	return gulp.src("path/to/**/*.php", {read: false})
+		.pipe(phpMinify(options))
+		.pipe(gulp.dest("path/to/out"));
+}
--- test/minifier_test.js
+++ test/minifier_test.js
@@ -1,37 +1,39 @@
-import {strict as assert} from 'assert';
-import File from 'vinyl';
-import {Minifier, TransformMode} from '../lib/index.js';
+import assert from "node:assert/strict";
+import File from "vinyl";
+import {Minifier, TransformMode} from "../lib/index.js";
 
-/** Tests the features of the `Minifier` class. */
-describe('Minifier', function() {
-  this.retries(3);
-  this.timeout(60000);
+/**
+ * Tests the features of the {@link Minifier} class.
+ */
+describe("Minifier", /** @this {Mocha.Suite} */ function() {
+	this.retries(3);
+	this.timeout(60_000);
 
-  describe('._transform()', () => {
-    it('should remove the comments and whitespace using the fast transformer', async () => {
-      const file = new File({path: 'test/fixtures/sample.php'});
-      const minifier = new Minifier({mode: TransformMode.fast, silent: true});
-      await minifier._transform(file);
-      minifier.emit('end');
+	describe("._transform()", () => {
+		it("should remove the comments and whitespace using the fast transformer", async () => {
+			const file = new File({path: "test/fixtures/sample.php"});
+			const minifier = new Minifier({mode: TransformMode.fast, silent: true});
+			await minifier._transform(file);
+			minifier.emit("end");
 
-      const contents = file.contents.toString();
-      assert(contents.includes("<?= 'Hello World!' ?>"));
-      assert(contents.includes('namespace dummy; class Dummy'));
-      assert(contents.includes('$className = get_class($this); return $className;'));
-      assert(contents.includes('__construct() { }'));
-    });
+			const contents = file.contents.toString();
+			assert(contents.includes("<?= 'Hello World!' ?>"));
+			assert(contents.includes("namespace dummy; class Dummy"));
+			assert(contents.includes("$className = get_class($this); return $className;"));
+			assert(contents.includes("__construct() { }"));
+		});
 
-    it('should remove the comments and whitespace using the safe transformer', async () => {
-      const file = new File({path: 'test/fixtures/sample.php'});
-      const minifier = new Minifier({mode: TransformMode.safe, silent: true});
-      await minifier._transform(file);
-      minifier.emit('end');
+		it("should remove the comments and whitespace using the safe transformer", async () => {
+			const file = new File({path: "test/fixtures/sample.php"});
+			const minifier = new Minifier({mode: TransformMode.safe, silent: true});
+			await minifier._transform(file);
+			minifier.emit("end");
 
-      const contents = file.contents.toString();
-      assert(contents.includes("<?= 'Hello World!' ?>"));
-      assert(contents.includes('namespace dummy; class Dummy'));
-      assert(contents.includes('$className = get_class($this); return $className;'));
-      assert(contents.includes('__construct() { }'));
-    });
-  });
+			const contents = file.contents.toString();
+			assert(contents.includes("<?= 'Hello World!' ?>"));
+			assert(contents.includes("namespace dummy; class Dummy"));
+			assert(contents.includes("$className = get_class($this); return $className;"));
+			assert(contents.includes("__construct() { }"));
+		});
+	});
 });
