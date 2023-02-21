4268af682c3d340486ed17eec98cef87b59280f6
Cédric Belin <cedric@belin.io>
2023-02-21 10:45:50 +0100

    Updated the "Lint" task [skip ci]

--- scripts/Lint.hx
+++ scripts/Lint.hx
@@ -3,6 +3,12 @@ import sys.io.File;
 
 /** Performs the static analysis of source code. **/
 function main() {
+	Sys.command("lix", ["run", "checkstyle",
+		"--config", "etc/checkstyle.json",
+		"--exitcode",
+		"--source", "scripts"
+	]);
+
 	final include = Json.parse(File.getContent("jsconfig.json")).include;
 	Sys.command("npx tsc --project jsconfig.json");
 	Sys.command("npx", ["eslint", "--config=etc/eslint.json"].concat(include));
