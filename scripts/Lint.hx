2872364809b85d27a69c221be30890bf5d7d6da4
Cédric Belin <cedric@belin.io>
2023-02-25 00:12:20 +0100

    Code formatting [skip ci]

--- scripts/Lint.hx
+++ scripts/Lint.hx
@@ -3,13 +3,8 @@ import sys.io.File;
 
 /** Performs the static analysis of source code. **/
 function main() {
-	Sys.command("lix", ["run", "checkstyle",
-		"--config", "etc/checkstyle.json",
-		"--exitcode",
-		"--source", "scripts"
-	]);
-
 	final include = Json.parse(File.getContent("jsconfig.json")).include;
+	Sys.command("lix run checkstyle --config etc/checkstyle.json --exitcode --source scripts");
 	Sys.command("npx tsc --project jsconfig.json");
 	Sys.command("npx", ["eslint", "--config=etc/eslint.json"].concat(include));
 }
