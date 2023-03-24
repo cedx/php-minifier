b974a56e9b3f5e39e4574fe66c57531741dc2822
Cédric Belin <cedric@belin.io>
2023-03-24 17:42:18 +0100

    Fixed the "Test" task [skip ci]

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,3 +1,3 @@
 /** Runs the test suite. **/
 function main()
-	Sys.command("npx c8 --all --include=src/**/*.js -report-dir=var --reporter=lcovonly node --test");
+	Sys.command("npx c8 --all --include=src/**/*.js --report-dir=var --reporter=lcovonly node --test");
