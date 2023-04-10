b68e4f0970d68bf1727073548897b43a42924179
Cédric Belin <cedric@belin.io>
2023-04-10 19:32:57 +0200

    Add the tests of the `SafeTransformer` class

---
+++ test/RunTests.hx
@@ -0,0 +1,18 @@
+import instrument.coverage.Coverage;
+import tink.testrunner.Reporter.AnsiFormatter;
+import tink.testrunner.Reporter.BasicReporter;
+import tink.testrunner.Runner;
+import tink.unit.TestBatch;
+
+/** Runs the test suite. **/
+function main() {
+	final tests = TestBatch.make([
+		new php_minify.SafeTransformerTest()
+	]);
+
+	ANSI.stripIfUnavailable = false;
+	Runner.run(tests, new BasicReporter(new AnsiFormatter())).handle(outcome -> {
+		Coverage.endCoverage();
+		Runner.exit(outcome);
+	});
+}
---
+++ test/php_minify/FastTransformerTest.hx
@@ -0,0 +1,8 @@
+package php_minify;
+
+/** Tests the features of the `FastTransformer` class. **/
+final class FastTransformerTest {
+
+	/** Creates a new test. **/
+	public function new() {}
+}
