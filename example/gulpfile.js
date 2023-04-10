5562e0c7945728e47ef0a5ca2df1269c27a6c01b
Cédric Belin <cedric@belin.io>
2023-04-10 14:12:43 +0200

    Rename the project

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,6 +1,6 @@
 import process from "node:process";
 import gulp from "gulp";
-import phpMinify from "@cedx/gulp-php-minify";
+import phpMinify from "@cedx/php-minify";
 
 /**
  * Compresses a given set of PHP scripts.
