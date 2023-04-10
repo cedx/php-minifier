d866422cc38a9c622cb9f427263706425ecaeb64
Cédric Belin <cedric@belin.io>
2023-04-10 15:54:34 +0200

    Add the skeleton of the fast/safe transformers

--- src/php_minify/Transformer.hx
+++ src/php_minify/Transformer.hx
@@ -1,19 +1,11 @@
 package php_minify;
 
-using haxe.io.Path;
-
 /** Removes comments and whitespace from a PHP script. **/
-abstract class Transformer {
-
-	/** The path to the PHP executable. **/
-	final executable: String;
-
-	/** Creates a new transformer. **/
-	function new(executable: String) this.executable = executable.normalize();
+interface Transformer {
 
 	/** Closes this transformer and releases any resources associated with it. **/
-	abstract function close(): Promise<Noise>;
+	function close(): Promise<Noise>;
 
 	/** Processes a PHP script. **/
-	abstract function transform(file: String): Promise<String>;
+	function transform(file: String): Promise<String>;
 }
