7270a4ce3569ea99f1ef7cc81b8dd87c8c5ff283
Cédric Belin <cedric@belin.io>
2023-04-11 16:59:04 +0200

    Add the externs of the "plugin-error" package

--- src/js/plugin_error/PluginError.hx
+++ src/js/plugin_error/PluginError.hx
@@ -1,5 +1,37 @@
 package js.plugin_error;
 
-/** TODO **/
+import haxe.extern.EitherType;
+import js.lib.Error;
+
+/** Represents a plugin error. **/
 @:jsRequire("plugin-error")
-extern class PluginError {}
+extern class PluginError {
+
+	/** Creates a new plugin error. **/
+	function new(plugin: String, message: EitherType<Error, String>, ?options: PluginErrorOptions);
+}
+
+/** Defines the options of a `PluginError` instance. **/
+typedef PluginErrorOptions = {
+
+	/** The file name where the error occurred. **/
+	var ?fileName: String;
+
+	/** The line number where the error occurred. **/
+	var ?lineNumber: Int;
+
+	/** The error message. **/
+	var ?message: Any;
+
+	/** The error name. **/
+	var ?name: String;
+
+	/** Value indicating whether to the error properties. **/
+	var ?showProperties: Bool;
+
+	/** Value indicating whether to the stack. **/
+	var ?showStack: Bool;
+
+	/** The error stack to use. **/
+	var ?stack: String;
+}
