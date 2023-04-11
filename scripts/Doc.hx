346ce7c93ea73001f19f795d40b207074eaa437c
Cédric Belin <cedric@belin.io>
2023-04-11 22:05:13 +0200

    Update the build system [skip ci]

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -1,7 +1,24 @@
+//! --class-path src --library tink_core
+import php_minify.Version;
+import sys.FileSystem;
 import sys.io.File;
 
 /** Builds the documentation. **/
 function main() {
-	Sys.command("npx typedoc --options etc/typedoc.json");
-	File.copy("www/favicon.ico", "docs/favicon.ico");
+	if (FileSystem.exists("docs/api")) Tools.removeDirectory("docs/api");
+
+	Sys.command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	Sys.command("lix", ["run", "dox",
+		"--define", "description", "Minify PHP source code by removing comments and whitespace.",
+		"--define", "source-path", "https://github.com/cedx/php-minify/blob/main/src",
+		"--define", "themeColor", "0x617cbe",
+		"--define", "version", Version.packageVersion,
+		"--define", "website", "https://docs.belin.io/php-minify",
+		"--input-path", "var",
+		"--output-path", "docs/api",
+		"--title", "PHP Minify",
+		"--toplevel-package", "php_minify"
+	]);
+
+	File.copy("docs/favicon.ico", "docs/api/favicon.ico");
 }
