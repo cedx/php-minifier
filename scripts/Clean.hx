91bd87e26ac1b0d83fb7981ed884ff0d8a9333ab
Cédric Belin <cedric@belin.io>
2023-04-12 20:16:13 +0200

    Update the build system

--- scripts/Clean.hx
+++ scripts/Clean.hx
@@ -3,7 +3,6 @@ using Lambda;
 
 /** Deletes all generated files. **/
 function main() {
-	for (file in ["bin/php_minify.js", "lib/index.js"]) [file, '$file.map'].filter(FileSystem.exists).iter(FileSystem.deleteFile);
-	["lib", "res"].filter(FileSystem.exists).iter(Tools.removeDirectory);
+	for (file in ["bin/php_minify.js", "lib/bundle.js"]) [file, '$file.map'].filter(FileSystem.exists).iter(FileSystem.deleteFile);
 	Tools.cleanDirectory("var");
 }
