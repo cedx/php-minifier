0c6281de4d05dacc79b704ba543a07bd85c054d9
Cédric Belin <cedric@belin.io>
2023-04-13 17:59:12 +0200

    Update the sample Gulp files

---
+++ example/gulpfile.cjs
@@ -0,0 +1,19 @@
+const process = require("node:process");
+const gulp = require("gulp");
+const phpMinify = require("@cedx/php-minify");
+
+/**
+ * Compresses a given set of PHP scripts.
+ */
+exports.default = function() {
+	const isWindows = process.platform == "win32";
+	const options = {
+		binary: isWindows ? "C:\\Program Files\\PHP\\php.exe" : "/usr/bin/php",
+		mode: isWindows ? "safe" : "fast",
+		silent: process.stdout.isTTY
+	};
+
+	return gulp.src("path/to/**/*.php", {read: false})
+		.pipe(phpMinify(options))
+		.pipe(gulp.dest("path/to/out"));
+}
