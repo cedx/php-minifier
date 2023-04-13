06775d9733bc7a3eb0743793581cd8c1f4ec54da
Cédric Belin <cedric@belin.io>
2023-04-13 12:04:39 +0200

    Improve the CLI usage

--- src/php_minify/Program.hx
+++ src/php_minify/Program.hx
@@ -1,11 +1,20 @@
 package php_minify;
 
+import sys.FileSystem;
 import tink.Cli;
 import tink.cli.Rest;
 using Lambda;
+using haxe.io.Path;
 
-/** Minify PHP source code by removing comments and whitespace. **/
+/**
+	Minify PHP source code by removing comments and whitespace.
+
+	> php_minify [flags] <input> [output]
+**/
 @:noDoc final class Program {
+
+	/** The extension of the PHP files to process. Defaults to "php". **/
+	public var extension = "php";
 
 	/** Display this help. **/
 	public var help = false;
@@ -20,8 +29,8 @@ using Lambda;
 	static function main() Cli.process(Sys.args(), new Program()).handle(Cli.exit);
 
 	/**
-		<input> : The input directory.
-		[output] : The output directory.
+		input  : The input directory.
+		output : The output directory.
 	**/
 	@:defaultCommand
 	public function run(rest: Rest<String>): Promise<Noise> {
@@ -30,9 +39,22 @@ using Lambda;
 			return Noise;
 		}
 
-		final requiredArgs = Sys.getEnv("HAXELIB_RUN") == "1" ? 2 : 1;
+		final haxelibRun = Sys.getEnv("HAXELIB_RUN") == "1";
+		final requiredArgs = haxelibRun ? 2 : 1;
 		if (rest.length < requiredArgs) return new Error(BadRequest, "You must provide the path to the input directory.");
+
+		final cwd = haxelibRun ? rest.pop() : Sys.getCwd();
+		final input = resolvePath(rest.shift(), cwd);
+		if (!FileSystem.exists(input)) return new Error(NotFound, "The input directory was not found.");
+		trace(input);
+
+		final output = rest.length > 0 ? resolvePath(rest.shift(), cwd) : input;
+		// TODO ??? FileSystem.createDirectory(output);
+		trace(output);
 
 		return Noise;
 	}
+
+	/** Resolves the specified `path` into an absolute path. **/
+	function resolvePath(path: String, cwd: String) return path.isAbsolute() ? path : Path.join([cwd, path]);
 }
