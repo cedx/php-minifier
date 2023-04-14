591112a7a15f4ed3ece055a626aa85fce01a09a6
Cédric Belin <cedric@belin.io>
2023-04-15 00:18:07 +0200

    Update the "Publish" task

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -3,9 +3,6 @@ import php_minifier.Version;
 
 /** Publishes the package. **/
 function main() {
-	Sys.command("lix Dist");
-	Tools.compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "run.n", "src"], "var/haxelib.zip");
-	Sys.command("haxelib submit var/haxelib.zip");
 	Sys.command("npm publish");
 	for (action in ["tag", "push origin"]) Sys.command('git $action v${Version.packageVersion}');
 }
