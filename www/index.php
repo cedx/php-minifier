974f80e961ea157a636058e28807be1b12554b6c
Cédric Belin <cedric@belin.io>
2023-08-10 15:52:07 +0200

    Improve compatibility with PHP 7

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
  * @param string $mediaType The response media type.
  * @param int $status The status code of the response.
  */
-function sendResponse(string $body, string $mediaType = "text/plain", int $status = 200): void {
+function sendResponse($body, $mediaType = "text/plain", $status = 200) {
 	http_response_code($status);
 	header("Content-Length: ".strlen($body));
 	header("Content-Type: $mediaType");
@@ -17,9 +17,9 @@ try {
 	if (empty($_GET["file"])) throw new LogicException("Bad Request", 400);
 	$output = @php_strip_whitespace($_GET["file"]);
 	if (!$output) throw new RuntimeException("Not Found", 404);
-	sendResponse($output, mediaType: "application/x-php");
+	sendResponse($output, "application/x-php", 200);
 }
 catch (Throwable $e) {
 	$code = $e->getCode();
-	sendResponse($e->getMessage(), status: $code >= 400 && $code < 600 ? $code : 500);
+	sendResponse($e->getMessage(), "text/plain", $code >= 400 && $code < 600 ? $code : 500);
 }
