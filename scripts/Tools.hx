ab7649918ef13174afb78e6aa61105c792061f72
Cédric Belin <cedric@belin.io>
2023-11-06 22:14:05 +0100

    Code formatting [skip ci]

--- scripts/Tools.hx
+++ scripts/Tools.hx
@@ -19,7 +19,7 @@ function compress(sources: Array<String>, destination: String) {
 	final output = File.write(destination);
 	final writer = new Writer(output);
 
-	var entries: Array<Entry> = [];
+	var entries = [];
 	for (source in sources) entries = entries.concat(FileSystem.isDirectory(source) ? compressDirectory(source) : [compressFile(source)]);
 	writer.write(entries.list());
 	output.close();
@@ -37,7 +37,7 @@ function replaceInFile(file: String, pattern: EReg, replacement: String)
 
 /** Compresses the content of the specified `directory` in ZIP format. **/
 private function compressDirectory(directory: String) {
-	var entries: Array<Entry> = [];
+	var entries = [];
 	for (entry in FileSystem.readDirectory(directory)) {
 		final path = Path.join([directory, entry]);
 		entries = entries.concat(FileSystem.isDirectory(path) ? compressDirectory(path) : [compressFile(path)]);
