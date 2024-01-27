31e0bdedca593c9fd4eed02e9bb5cafba5a73b39
Cédric Belin <cedric@belin.io>
2024-01-27 22:57:58 +0100

    Code factorization [skip ci]

--- www/index.php
+++ www/index.php
@@ -1,5 +1,16 @@
 <?php
 /**
+ * Application entry point.
+ * @param array<string, string> $query The query string.
+ */
+function main(array $query): void {
+	if (empty($query["file"])) throw new LogicException("Bad Request", 400);
+	$output = @php_strip_whitespace($query["file"]);
+	if (!$output) throw new RuntimeException("Not Found", 404);
+	sendResponse($output, "application/x-php", 200);
+}
+
+/**
  * Sends the specified response to the client.
  * @param string $body The response body.
  * @param string $mediaType The response media type.
@@ -13,12 +24,7 @@ function sendResponse(string $body, string $mediaType = "text/plain", int $statu
 }
 
 // Start the application.
-try {
-	if (empty($_GET["file"])) throw new LogicException("Bad Request", 400);
-	$output = @php_strip_whitespace($_GET["file"]);
-	if (!$output) throw new RuntimeException("Not Found", 404);
-	sendResponse($output, "application/x-php", 200);
-}
+try { main($_GET); }
 catch (Throwable $e) {
 	$code = $e->getCode();
 	sendResponse($e->getMessage(), "text/plain", $code >= 400 && $code < 600 ? $code : 500);
