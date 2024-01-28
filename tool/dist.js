23c75e59d2875478c75507fba5926aaa7fc8cebc
Cédric Belin <cedric@belin.io>
2024-01-28 13:04:53 +0100

    Optimize the build task

--- tool/dist.js
+++
@@ -1,6 +0,0 @@
-import {readFileSync, writeFileSync} from "node:fs";
-import {EOL} from "node:os";
-
-// Packages the project.
-const file = "bin/php_minifier.cjs";
-writeFileSync(file, `#!/usr/bin/env node${EOL}${readFileSync(file, {encoding: "utf8"})}`);
