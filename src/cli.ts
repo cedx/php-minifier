443f99bf74838f931e38ee85f8533f8c52a9b509
Cédric Belin <cedric@belin.io>
2024-01-30 02:00:51 +0100

    Update the dependencies

--- src/cli.ts
+++ src/cli.ts
@@ -4,9 +4,32 @@ import {dirname, join, resolve} from "node:path";
 import process from "node:process";
 import {parseArgs} from "node:util";
 import readdirp from "readdirp";
-import pkg from "../../package.json" with {type: "json"};
-import usage from "./usage.js";
-import {FastTransformer, SafeTransformer, TransformMode} from "../index.js";
+import pkg from "../package.json" with {type: "json"};
+import {FastTransformer} from "./fast_transformer.js";
+import {SafeTransformer} from "./safe_transformer.js";
+import {TransformMode} from "./transform_mode.js";
+
+/**
+ * The usage information.
+ */
+const usage = `
+Minify PHP source code by removing comments and whitespace.
+
+Usage:
+  php_minifier [options] <input> <output>
+
+Arguments:
+  input            The path to the input directory.
+  output           The path to the output directory.
+
+Options:
+  -b, --binary     The path to the PHP executable.
+  -e, --extension  The extension of the PHP files to process. Defaults to "php".
+  -m, --mode       The operation mode of the minifier. Defaults to "safe".
+  -s, --silent     Value indicating whether to silence the minifier output.
+  -h, --help       Display this help.
+  -v, --version    Output the version number.
+`;
 
 /**
  * Defines the command line options.
