3d47fec17a66a4acd0194276cd67836e5bd6b626
Cédric Belin <cedric@belin.io>
2024-08-30 13:05:35 +0200

    Add the test entry point

---
+++ test/RunTests.hx
@@ -0,0 +1,16 @@
+import tink.testrunner.Reporter.AnsiFormatter;
+import tink.testrunner.Reporter.BasicReporter;
+import tink.testrunner.Runner;
+import tink.unit.TestBatch;
+
+/** Runs the test suite. **/
+function main() {
+	final tests = TestBatch.make([
+		new php_minifier.FastTransformerTest(),
+		new php_minifier.GulpPluginTest(),
+		new php_minifier.SafeTransformerTest()
+	]);
+
+	ANSI.stripIfUnavailable = false;
+	Runner.run(tests, new BasicReporter(new AnsiFormatter())).handle(Runner.exit);
+}
