664e9bff49814aa5096b0984d39f57802a846e44
Cédric Belin <cedric@belin.io>
2024-08-31 01:23:25 +0200

    Fix the safe transformer

--- src/php_minifier/SafeTransformer.hx
+++ src/php_minifier/SafeTransformer.hx
@@ -1,9 +1,8 @@
 package php_minifier;
 
 import asys.FileSystem;
-import asys.io.Process;
+import js.node.ChildProcess;
 using haxe.io.Path;
-using tink.io.Source;
 
 /** Removes comments and whitespace from a PHP script, by calling a PHP process. **/
 class SafeTransformer implements Transformer {
@@ -12,17 +11,17 @@ class SafeTransformer implements Transformer {
 	final executable: String;
 
 	/** Creates a new safe transformer. **/
-	public function new(executable = "php") this.executable = executable.normalize();
+	public function new(executable = "php")
+		this.executable = executable.normalize();
 
 	/** Closes this transformer and releases any resources associated with it. **/
 	public function close(): Promise<Noise>
 		return Noise;
 
 	/** Processes a PHP script. **/
-	public function transform(file: String): Promise<String> {
-		final process = new Process(executable, ["-w", FileSystem.absolutePath(file)]);
-		return process.exitCode()
-			.next(exitCode -> exitCode == 0 ? process.stdout.all() : new Error(exitCode, 'The PHP process exited with a $exitCode code.'))
-			.next(stdout -> { process.close(); stdout.toString(); });
-	}
+	public function transform(file: String): Promise<String>
+		return Promise.irreversible((resolve, reject) -> {
+			final callback = (error, stdout, stderr) -> error != null ? reject(Error.ofJsError(error)) : resolve(stdout);
+			ChildProcess.execFile(executable, ["-w", FileSystem.absolutePath(file)], {maxBuffer: 20 * 1_024 * 1_024}, callback);
+		});
 }
