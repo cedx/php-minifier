706d41be343f5449f9ec6004cf585f0269b2de6e
Cédric Belin <cedric@belin.io>
2024-09-19 19:27:51 +0200

    Enable the source maps

--- bin/php_minifier.ps1
+++ bin/php_minifier.ps1
@@ -1,3 +1,6 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
-node "$PSScriptRoot/php_minifier.js" @args
+
+$file = "$PSScriptRoot/php_minifier.js"
+if (Test-Path "$file.map") { node --enable-source-maps $file @args }
+else { node $file @args }
