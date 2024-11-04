fb4bbb01255bf35b45e6043368f0d825332198b8
Cédric Belin <cedric@belin.io>
2024-11-04 15:23:48 +0100

    Port the build system to Cake

--- scripts/Build.hx
+++
@@ -1,5 +0,0 @@
-/** Builds the project. **/
-function main() {
-	final debug = Sys.args().contains("--debug");
-	for (file in ["build", "run"]) Sys.command('haxe ${debug ? "--debug" : ""} $file.hxml');
-}
--- scripts/Dist.hx
+++
@@ -1,7 +0,0 @@
-/** Packages the project. **/
-function main() {
-	final cli = "bin/php_minifier.js";
-	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
-	for (file in [cli, "lib/bundle.js"]) Sys.command('npx terser --comments=false --compress --mangle --output=$file $file');
-	Sys.command('git update-index --chmod=+x $cli');
-}
