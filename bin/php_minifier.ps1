5abf48ec68facaeaa6ac71a7463475f0237a9963
Cédric Belin <cedric@belin.io>
2024-11-04 14:42:35 +0100

    Update the CLI entry script

--- bin/php_minifier.ps1
+++ bin/php_minifier.ps1
@@ -2,5 +2,5 @@
 Set-StrictMode -Version Latest
 
 $file = "$PSScriptRoot/php_minifier.js"
-if (Test-Path "$file.map") { node --enable-source-maps $file @args }
+if (Test-Path "$PSScriptRoot/../lib/cli.js.map") { node --enable-source-maps $file @args }
 else { node $file @args }
