b2c0c18f8a4d110f99892403d337018fa71f0de6
Cédric Belin <cedric@belin.io>
2024-11-04 15:16:39 +0100

    Add the TypeScript settings

---
+++ example/gulpfile.js
@@ -0,0 +1,17 @@
+import {phpMinify} from "@cedx/php-minifier";
+import gulp from "gulp";
+import process from "node:process";
+
+// Compresses a given set of PHP scripts.
+export function compressPhp() {
+	const isWindows = process.platform == "win32";
+	const options = {
+		binary: isWindows ? "C:/Program Files/PHP/php.exe" : "/usr/bin/php",
+		mode: isWindows ? "safe" : "fast",
+		silent: process.stdout.isTTY
+	};
+
+	return gulp.src("path/to/**/*.php", {read: false})
+		.pipe(phpMinify(options))
+		.pipe(gulp.dest("path/to/out"));
+}
