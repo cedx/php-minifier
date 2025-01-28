8e302bceee96b9930718dfda3599159643d638fc
Cédric Belin <cedric@belin.io>
2025-01-28 01:33:14 +0100

    Move the launch script

--- bin/php_minifier.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-
-$file = "$PSScriptRoot/php_minifier.js"
-if (Test-Path "$PSScriptRoot/../lib/cli.js.map") { node --enable-source-maps $file @args }
-else { node $file @args }
---
+++ run.ps1
@@ -0,0 +1,5 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$file = "$PSScriptRoot/lib/cli.js"
+if (Test-Path "$file.map") { & node --enable-source-maps $file @args }
+else { & node $file @args }
