189264efc294baf5b2bde66fe90cb6d657d03cfb
Cédric Belin <cedric@belin.io>
2025-01-31 02:16:11 +0100

    Add the API reference

---
+++ etc/typedoc.js
@@ -0,0 +1,11 @@
+/** @type {Partial<import("typedoc").TypeDocOptions>} */
+export default {
+	entryPoints: ["../src/index.ts"],
+	excludePrivate: true,
+	gitRevision: "main",
+	hideGenerator: true,
+	name: "PHP Minifier",
+	out: "../docs",
+	readme: "none",
+	tsconfig: "../src/tsconfig.json"
+};
