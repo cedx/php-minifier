2f0a5b77148d2055796376b198f63fed20dace55
Cédric Belin <cedric@belin.io>
2025-05-11 23:02:46 +0200

    Naming convention

--- etc/TypeDoc.js
+++ etc/TypeDoc.js
@@ -2,7 +2,7 @@
  * @type {Partial<import("typedoc").TypeDocOptions>}
  */
 export default {
-	entryPoints: ["../src/index.ts"],
+	entryPoints: ["../src/Main.ts"],
 	excludePrivate: true,
 	gitRevision: "main",
 	hideGenerator: true,
