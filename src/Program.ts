4477f7e7df7235abe40dbf420667ae861b57d575
Cédric Belin <cedx@outlook.com>
2025-07-28 23:59:26 +0200

    Fix the usage

--- src/Program.ts
+++ src/Program.ts
@@ -22,7 +22,8 @@ Options:
 	-b, --binary     The path to the PHP executable.
 	-e, --extension  The extension of the PHP files to process. Defaults to "php".
 	-m, --mode       The operation mode of the minifier. Defaults to "safe".
-	-s, --silent     Value indicating whether to silence the minifier output.
+	-r, --recursive  Whether to process the input directory recursively.
+	-s, --silent     Whether to silence the minifier output.
 	-h, --help       Display this help.
 	-v, --version    Output the version number.
 `;
